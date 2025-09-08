44d990738097ca0b1b0b48cc16af4942709a6643
Siddharth Shrivastav <siddharthshrivastav@Siddharths-MacBook-Air.local>
2025-09-08 16:32:35 +0530

    new assignment answers

--- ReadME.md
+++ ReadME.md
@@ -1,1 +1,3 @@
 REPOSITORY FOR AZURE TRAINING
+
+Did the batch_processing problem all the relevant files are added to the repository
--- batch_processing.py
+++ batch_processing.py
@@ -1,10 +1,13 @@
 # First creating the file
 import csv
 import random
-
+from datetime import datetime, timedelta
 import pandas as pd
 
 headers = [["order_id","customer_id","order_date","product","quantity","price"]]
+
+start = datetime(2022, 1, 1).date()
+end = datetime(2022, 12, 31).date()
 
 with open("sales_data.csv", "w+") as file:
     writer = csv.writer(file)
@@ -12,9 +15,11 @@ with open("sales_data.csv", "w+") as file:
     writer.writerows(headers)
 
     for i in range(1,51):
+        delta = end - start
+        random_days = random.randint(0, delta.days)
         data = [[f"order_{random.choice(range(1,51))}",
                 f"customer_{random.choice(range(1,51))}",
-                "date",
+                f"{start + timedelta(days=random_days)}",
                 f"product_{random.choice(range(1,51))}",
                 f"{random.choice(range(1,1001))}",
                 f"{random.choice(range(1,2001))}"]]
@@ -25,6 +30,25 @@ with open("sales_data.csv", "w+") as file:
 
 
 df = pd.read_csv("sales_data.csv")
-print(df)
 
-print(df.info())
+df = df.dropna()
+df = df.drop_duplicates()
+
+print(df.describe().T)
+
+# Finding revenue for all products
+df["revenue"] = df["quantity"] * df["price"]
+revenue_per_product = df.groupby("product")["revenue"].sum()
+revenue_per_product.to_csv("product_revenue.csv")
+
+# Top 5 customers by spending
+top_customers = df.groupby("customer_id")["revenue"].sum()
+ordered_customers = top_customers.sort_values(ascending = False).head(5)
+ordered_customers.to_csv("top_customers.csv")
+
+
+# Count how many orders where places each month
+df["order_date"] = pd.to_datetime(df["order_date"])
+df["month"] = df["order_date"].dt.month
+orders_perMonth = df.groupby("month")["revenue"].sum()
+orders_perMonth.to_csv("monthly_order.csv")

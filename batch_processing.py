8fb6ee0ebb47b22cd576cb66194077eaaf144c46
Siddharth Shrivastav <siddharthshrivastav@Siddharths-MacBook-Air.local>
2025-09-08 15:43:07 +0530

    New files

---
+++ batch_processing.py
@@ -0,0 +1,23 @@
+# First creating the file
+import csv
+import random
+
+
+headers = [["order_id","customer_id","order_date","product","quantity","price"]]
+
+with open("sales_data.csv", "w+") as file:
+    writer = csv.writer(file)
+
+    writer.writerows(headers)
+
+    for i in range(1,51):
+        data = [[f"order_{random.choice(range(1,51))}",
+                f"customer_{random.choice(range(1,51))}",
+                "date",
+                f"product_{random.choice(range(1,51))}",
+                f"{random.choice(range(1,1001))}",
+                f"{random.choice(range(1,2001))}"]]
+
+        print(data)
+
+        writer.writerows(data)
